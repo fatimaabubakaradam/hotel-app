fed9b3fc86561919b9f4221f0c1975e878d39a43
fatimaabubakaradam <zahrammcy@gmail.com>
2024-10-23 12:10:35 -0700

    add javascript

--- sidemenu.js
+++ sidemenu.js
@@ -1,43 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const menuToggle = document.getElementById('menuToggle');
-//     const closeMenu = document.getElementById('myCloseMenu');
-//     const navMenu = document.getElementById('navMenu');
 
-//     console.log(menuToggle, closeMenu, navMenu);
-
-//     if (menuToggle && navMenu && closeMenu) {
-        
-//         menuToggle.addEventListener('click', () => {
-//             console.log('Menu opened'); 
-//             navMenu.classList.add('active'); 
-//         });
-//     }
-
-    
-//         closeMenu.addEventListener('click', () => {
-//             console.log('Menu closed'); 
-//             navMenu.classList.remove('active');
-//         });
-
-  
-// });
-
-
-    // const menuToggle = document.getElementById('menuToggle');
-    // // const closeMenu = document.getElementById('myCloseMenu');
-
-    // menuToggle.addEventListener('click', () => {
-    //     console.log('Menu opened'); 
-    // });
-
-
-
-    // console.log(closeMenu)
-    // closeMenu.addEventListener('click', () => {
-    //     console.log('Menu closed'); 
-    //     navMenu.classList.add('remove');
-    // });
-    // const myCloseMenu = document.getElementById('menuClose')
     const closeMenu = () => {
         console.log('Hi')
         navMenu.classList.remove('active'); 
