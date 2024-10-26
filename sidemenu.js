c6fec56285726a91cae54b90c3195bbd7f4183a4
fatimaabubakaradam <zahrammcy@gmail.com>
2024-10-25 17:06:21 -0700

    update javascript

--- sidemenu.js
+++ sidemenu.js
@@ -1,11 +1,16 @@
-const navMenu = document.getElementById('navMenu');
+// const navMenu = document.getElementById('navMenu');
 
-const openMenu = () => {
-    console.log("Hello");
-    navMenu.classList.add('open');  
-}
+// const openMenu = () => {
+//     console.log("Hello");
+//     navMenu.classList.add('open');  
+// }
 
-const closeMenu = () => {
-    console.log("Hi");
-    navMenu.classList.remove('open');  
-}
+// const closeMenu = () => {
+//     // console.log("Hi");
+//     navMenu.classList.remove('show');  
+// }
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('visible');
+  });
